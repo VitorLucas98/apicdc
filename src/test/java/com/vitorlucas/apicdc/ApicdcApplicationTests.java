5e5d943377c42689417e32a78203ee1dd8fe6d78
Vitor Lucas <vitorluc.2013@gmail.com>
2021-05-01 21:08:09 -0300

    Created project

---
+++ src/test/java/com/vitorlucas/apicdc/ApicdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorlucas.apicdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
